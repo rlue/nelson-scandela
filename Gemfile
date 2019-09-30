fae8a35c883afbd068fbc51d914b02c8a783e4f0
Ryan Lue <hello@ryanlue.com>
2019-10-01 07:45:49 +0800

    Switch from SVG to HTML output

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.7.0'
 gem 'barby'
-gem 'cairo'
 gem 'sinatra'
 
 group :development do
--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 require 'barby'
 require 'barby/barcode/code_128'
-require 'barby/outputter/cairo_outputter'
+require 'barby/outputter/html_outputter'
 
 get '/:id' do
-  Barby::Code128.new(params['id']).to_svg
+  <<~HTML
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Nelson Scandela</title>
+        <style type="text/css" media="screen">
+          body { margin: 2vh 10vw; }
+
+          table.barby-barcode {
+            border-spacing: 0;
+            margin: 0 auto;
+            height: 96vh;
+          }
+
+          td.barby-cell { width: 3px; }
+          td.barby-cell.on { background-color: #000; }
+        </style>
+      </head>
+      <body>#{Barby::Code128.new(params['id']).to_html}</body>
+    </html>
+  HTML
 end
